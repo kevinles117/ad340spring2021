bfdec779fd7831d5b74ba440a3cba9885f67c046
Kevin Le <kevinles117@yahoo.com>
2021-06-01 21:08:08 -0700

    Added matches and settings fragment while also adding the navigation drawer and app bar

--- app/src/main/java/com/example/helloworld/entity/Settings.java
+++ app/src/main/java/com/example/helloworld/entity/Settings.java
@@ -4,11 +4,65 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import com.google.type.DateTime;
+
 @Entity
 public class Settings {
     @PrimaryKey
-    private int id;
+    private int id = 0;
 
-//    @ColumnInfo (name = "matches_reminder_time")
-//    private
+    @ColumnInfo(name = "matches_reminder_time")
+    private DateTime date;
+
+    @ColumnInfo(name = "maximum_distance_search")
+    private int distance;
+
+    @ColumnInfo(name = "gender")
+    private String gender;
+
+    @ColumnInfo(name = "account_privacy")
+    private Boolean privacy;
+
+    @ColumnInfo(name = "interested_age_range")
+    private String ageRange;
+
+    public DateTime getDate() {
+        return date;
+    }
+
+    public void setDate(DateTime date) {
+        this.date = date;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Boolean getPrivacy() {
+        return privacy;
+    }
+
+    public void setPrivacy(Boolean privacy) {
+        this.privacy = privacy;
+    }
+
+    public String getAgeRange() {
+        return ageRange;
+    }
+
+    public void setAgeRange(String ageRange) {
+        this.ageRange = ageRange;
+    }
 }
